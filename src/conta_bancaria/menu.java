a574c50ae80a79a7a6735cb1dafee3c21900ba5f
guitxc <guisandolidna@gmail.com>
2026-06-19 15:10:41 -0300

    :sparkles: Atualizando a feature Menu

--- src/conta_bancaria/menu.java
+++ src/conta_bancaria/menu.java
@@ -1,10 +1,49 @@
 package conta_bancaria;
 
-public class menu {
+import conta_bancaria.util.Cores;
+
+public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*****************************************************************************************************");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                                                                                   *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                       BANCO DO BRASIL                                             *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                                                                                   *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*****************************************************************************************************");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                                                                                   *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                       1 - Criar Conta                                             *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                       2 - Listar todas as Contas                                  *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                       3 - Buscar Conta por Numero                                 *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                       4 - Atualizar Dados da Conta                                *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                       5 - Apagar Conta                                            *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                       6 - Sacar                                                   *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                       7 - Depositar                                               *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                       8 - Transferir valores entre Contas                         *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                       0 - Sair                                                    *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                                                                                   *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*****************************************************************************************************");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                       Escolha a Opção desejada                                    *");
+		System.out.println(Cores.ANSI_WHITE_BACKGROUND + Cores.TEXT_RED_UNDERLINED
+				+ "*                                                                                                   *");
 	}
-
 }
